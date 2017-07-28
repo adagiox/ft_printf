8518e8bc979c4aed183c1a85ff723139b8d8da90
adagiox <gerfiglmac@gmail.com>
2017-07-27 23:18:37 -0700

    Added some basic parsing

--- srcs/ft_parseformat.c
+++ srcs/ft_parseformat.c
@@ -1,6 +1,9 @@
 #include "ft_printf.h"
 
-t_printf parse_format(const char *format)
+int parse_format(const char *format, va_list args)
 {
-	t_printf parse;
+	t_printf *print_list;	// pointer to the head of the list of parsed argument
+							// ready to be converted
+	
+	list = parse_subftm(format, list);
 }
